219b9b4ebacd5ce9189cb373bb136cdbf3eb8b74
Tim (Bobby-Tablez) Peck <git@pf5.co>
2022-03-29 17:42:03 -0600

    Initial Uploat
    This PowerShell script scans popular browsers on Windows operating system and outputs the version. Currently supported browsers are:
    - Google Chrome
    - Microsoft Edge
    - Brave Browser
    - Mozilla Firefox

---
+++ PowerShell/browser_versions.ps1
@@ -0,0 +1,31 @@
+﻿# Check Chrome Version
+try {
+    $ChromeBin = "C:\Program Files\Google\Chrome\Application\chrome.exe"
+    $ChromeVersion = [System.Diagnostics.FileVersionInfo]::GetVersionInfo($ChromeBin).ProductVersion
+    Write-Output "Google Chrome Version: " $ChromeVersion "`n"
+}
+Catch { Write-Output "Chrome Not Installed`n" }
+
+# Check Brave Version
+try {
+    $BraveBin = "C:\Program Files\BraveSoftware\Brave-Browser\Application\brave.exe"
+    $BraveVersion = [System.Diagnostics.FileVersionInfo]::GetVersionInfo($BraveBin).ProductVersion
+    Write-Output "Brave Browser Version: " $BraveVersion "`n"
+}
+Catch { Write-Output "Brave Not Installed`n" }
+
+# Check Edge Version
+try {    
+    $EdgeBin = "C:\Program Files (x86)\Microsoft\Edge\Application\msedge.exe"
+    $EdgeVersion = [System.Diagnostics.FileVersionInfo]::GetVersionInfo($EdgeBin).ProductVersion
+    Write-Output "Microsoft Edge version: "  $EdgeVersion "`n"
+}
+Catch { Write-Output "Edge Not Installed`n" }
+
+# Check FireFox Version
+try {    
+    $FirefoxBin = "C:\Program Files\Mozilla Firefox\firefox.exe"
+    $FirefoxVersion = [System.Diagnostics.FileVersionInfo]::GetVersionInfo($FirefoxBin).ProductVersion
+    Write-Output "Firefox version: "  $FirefoxVersion "`n"
+}
+Catch { Write-Output "Firefox Installed`n" }
