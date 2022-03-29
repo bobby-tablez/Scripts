803b7b23a07523a11b54df41939dd4e5a4135bfc
Tim (Bobby-Tablez) Peck <git@pf5.co>
2022-03-29 17:43:31 -0600

    Update browser_versions.ps1

--- PowerShell/browser_versions.ps1
+++ PowerShell/browser_versions.ps1
@@ -1,4 +1,5 @@
-﻿# Check Chrome Version
+﻿# This script scans popular web browsers on Windows OS and outputs the version of each. Currently supported browsers are: Microsoft Edge, Google Chrome, Brave Browser, and Mozilla Firefox.
+ # Check Chrome Version
 try {
     $ChromeBin = "C:\Program Files\Google\Chrome\Application\chrome.exe"
     $ChromeVersion = [System.Diagnostics.FileVersionInfo]::GetVersionInfo($ChromeBin).ProductVersion
